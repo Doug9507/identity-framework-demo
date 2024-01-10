42bdcbfe9dca3d3fd142af4d8e478c7d25cb4d4b
jaime perez <jaime.perez@intelica.com>
2024-01-10 17:25:01 -0500

    Identity implementation base

--- HuellitasIdentity/Program.cs
+++ HuellitasIdentity/Program.cs
@@ -1,4 +1,14 @@
+using HuellitasIdentity.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+
+//Identity configuration
+var connectionString = builder.Configuration.GetConnectionString("RazorPagesPetAuthConnection");
+builder.Services.AddDbContext<RazorPagesPetAuth>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<RazorPagesPetAuth>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -13,7 +23,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
-
+//Identity configuration
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
